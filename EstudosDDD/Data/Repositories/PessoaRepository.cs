76a421df5bb29e7890fef9d7d84a337e861e9f31
Laerte <laerte.uliam@gmail.com>
2015-07-29 17:27:29 -0300

    PessoaRepository

---
+++ EstudosDDD/Data/Repositories/PessoaRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EstudosDDD.Data.Contexts;
+using EstudosDDD.Domain.Entities;
+
+namespace EstudosDDD.Data.Repositories
+{
+    public sealed class PessoaRepository : Domain.Contracts.Repositories.IRepositoryPessoa
+    {
+        private readonly EstudosDDDContext _context;
+
+        public PessoaRepository(EstudosDDDContext context)
+        {
+            _context = context;
+        }
+
+        public int Add(PessoaEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PessoaEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
