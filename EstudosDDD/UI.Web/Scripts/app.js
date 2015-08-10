96145d424f783cd849e392dfcf3247df2778911c
Laerte <laerte.uliam@gmail.com>
2015-08-10 12:14:34 -0300

    AngularJs App

---
+++ EstudosDDD/UI.Web/Scripts/app.js
@@ -0,0 +1,10 @@
+﻿
+    var app = angular.module('EstudosDDDApp', function (){});
+
+    app.controller('PessoaController', function () {
+        this.setModels = function (valor) {
+            console.log(valor);
+            this.Models = JSON.parse(valor);
+        };
+
+    });
---
+++ EstudosDDD/UI.Web/Views/Pessoa/HtmlHelperExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace EstudosDDD.UI.Web.Views.Pessoa
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlString ToJson(this HtmlHelper htmlHelper, object model)
+        {
+            var serializer = new JavaScriptSerializer();
+            var json = serializer.Serialize(model);
+            return htmlHelper.Raw(json);
+        }
+    }
+}
