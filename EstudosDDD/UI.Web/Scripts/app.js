1b59fe489a473b8d3437e301b1fff1e05e68e96a
Laerte <laerte.uliam@gmail.com>
2015-08-11 15:35:57 -0300

    AngularJs prototype

--- EstudosDDD/UI.Web/Scripts/app.js
+++ EstudosDDD/UI.Web/Scripts/app.js
@@ -1,48 +1,41 @@
-﻿
-    var app = angular.module('EstudosDDDApp', function (){});
+﻿var app = angular.module('EstudosDDDApp', []);
+app.controller('PessoaController', PessoaController);
 
-    app.controller('PessoaController', function ($http) {
-        this.Items = {};
-        this.Editable = false;
-        this.itemSelecionado = '';
+function PessoaController($http) {
+    this.Items = {};
+    this.Editable = false;
+    this.ItemSelecionado = {};
 
-        this.salvar = function() {
-            alert('Salvo com sucesso.');
-        };
-
-        this.isEdit = function() {
-            return this.Editable;
-        };
-
-        this.edit = function(index) {
-            this.select(index);
-            this.Editable = true;
-        };
-
-        this.select = function (index) {
-            this.itemSelecionado = this.Items[index];
-            this.Editable = false;
-        };
-
-        this.isSelected = function() {
-            return (this.itemSelecionado != undefined);
-        };
-
-        this.panelTitle = function() {
-            if (this.isEdit())
-                return "Editar";
-            else
-                return "Visualizar";
-        };
-        
-        this.iniciar = function () {
-            $http.post('/Pessoa/Listar').
-            then(function (response) {
-                this.Items = response.data;
-            }, function (response) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-            });
-        };
-
+    $http.post('/Pessoa/Listar').
+    then(function (response) {
+        this.Items = response.data;
+    }, function (response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
     });
+};
+PessoaController.$inject = ['$http'];
+
+PessoaController.prototype.salvar = function () {
+    alert('Salvo com sucesso.');
+};
+PessoaController.prototype.isEdit = function () {
+    return this.Editable;
+};
+PessoaController.prototype.edit = function (index) {
+    this.select(index);
+    this.Editable = true;
+};
+PessoaController.prototype.select = function (index) {
+    this.ItemSelecionado = this.Items[index];
+    this.Editable = false;
+};
+PessoaController.prototype.isSelected = function() {
+    return (this.ItemSelecionado != undefined);
+};
+PessoaController.prototype.panelTitle = function() {
+    if (this.isEdit())
+        return "Editar";
+    else
+        return "Visualizar";
+};
