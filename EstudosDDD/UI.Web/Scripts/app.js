df9e94570520d297df0c9e91d53c949115997c38
Laerte <laerte.uliam@gmail.com>
2015-08-11 16:32:18 -0300

    Angular $scope ajustes

--- EstudosDDD/UI.Web/Scripts/app.js
+++ EstudosDDD/UI.Web/Scripts/app.js
@@ -1,41 +1,43 @@
 ﻿var app = angular.module('EstudosDDDApp', []);
-app.controller('PessoaController', PessoaController);
+app.controller('PessoaController', function ($scope, $http) {
+    $scope.Items = {};
+    $scope.Editable = false;
+    $scope.ItemSelecionado = undefined;
 
-function PessoaController($http) {
-    this.Items = {};
-    this.Editable = false;
-    this.ItemSelecionado = {};
+    $scope.dataBind = function() {
+        $http.post('/Pessoa/Listar').
+        then(function (response) {
+            $scope.Items = response.data;
+        }, function (response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    };
+    $scope.adicionar = function() {
+        $scope.ItemSelecionado = undefined;
+        $scope.Editable = true;
+    };
+    $scope.salvar = function () {
+        alert('Salvo com sucesso.');
+    };
+    $scope.isEdit = function () {
+        return $scope.Editable;
+    };
+    $scope.edit = function (index) {
+        $scope.select(index);
+        $scope.Editable = true;
+    };
+    $scope.select = function (index) {
+        $scope.ItemSelecionado = $scope.Items[index];
+        $scope.Editable = false;
+    };
+    $scope.isSelected = function () {
+        return ($scope.ItemSelecionado != undefined);
+    };
+    $scope.panelTitle = function () {
+        if ($scope.isEdit() && $scope.ItemSelecionado!=undefined) return "Editar";
+        if ($scope.isEdit() && $scope.ItemSelecionado == undefined) return "Adicionar";
+        if (!$scope.isEdit() && $scope.ItemSelecionado != undefined) return "Visualizar";
+    };
+});
 
-    $http.post('/Pessoa/Listar').
-    then(function (response) {
-        this.Items = response.data;
-    }, function (response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-    });
-};
-PessoaController.$inject = ['$http'];
-
-PessoaController.prototype.salvar = function () {
-    alert('Salvo com sucesso.');
-};
-PessoaController.prototype.isEdit = function () {
-    return this.Editable;
-};
-PessoaController.prototype.edit = function (index) {
-    this.select(index);
-    this.Editable = true;
-};
-PessoaController.prototype.select = function (index) {
-    this.ItemSelecionado = this.Items[index];
-    this.Editable = false;
-};
-PessoaController.prototype.isSelected = function() {
-    return (this.ItemSelecionado != undefined);
-};
-PessoaController.prototype.panelTitle = function() {
-    if (this.isEdit())
-        return "Editar";
-    else
-        return "Visualizar";
-};
